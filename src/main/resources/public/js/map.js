3b4fc598918db707dfe3e07a903cdbcaf10f467d
Cesar Guerrero <cesarguerrero@Cesars-MacBook-Pro.local>
2016-08-11 00:04:22 -0500

    sleep experiment

--- src/main/resources/public/js/map.js
+++ src/main/resources/public/js/map.js
@@ -121,7 +121,11 @@ function loadViewportMarkers() {
 			console.log(responseObject);
 			console.log(i);
 		});
+
+		sleep(1000);
 	}
+
+
 
 	// console.log(tiles);
 
@@ -130,4 +134,13 @@ function loadViewportMarkers() {
 	/* TODO: cache server results */
 
 	/* TODO: display viewport markers */
+}
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
 }
