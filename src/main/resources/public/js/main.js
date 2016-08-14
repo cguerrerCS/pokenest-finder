aaec86b90c8f72f7e1d3f2015bd28e7e2bdb22af
Cesar Guerrero <cesarguerrero@Cesars-MacBook-Pro.local>
2016-08-14 01:46:03 -0500

    opaque again

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -487,9 +487,9 @@ function onLocationFound(e) {
 		loadViewportMarkers();
 		// draw search radius
 		var circle = L.circle([newLatLng.lat, newLatLng.lng], (SEARCH_RADIUS * 1609), {
-    		color: '#E57373',
-    		fillColor: '#FFCDD2',
-    		fillOpacity: 0.7
+    		color: '#EF5350',
+    		fillColor: '#EF5350',
+    		fillOpacity: 0.3
 		}).addTo(pokemap);
 	}
 }
