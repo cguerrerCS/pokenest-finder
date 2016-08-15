e711aaa940ffdef5ac4b7116c345d8da306d3dcb
Cesar Guerrero <cesarguerrero@Cesars-MacBook-Pro.local>
2016-08-15 02:14:37 -0500

    backtracking

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -152,14 +152,14 @@ $( document ).ready(function() {
     	accessToken: 'pk.eyJ1IjoiY2d1ZXJyZXIiLCJhIjoiY2lxdmlzYmgxMDAxM2Z2bThvcm9kNGx1YiJ9.GdNs-_3lu5C2HrTqEbYGWg'
 	}).addTo(sitemap);
 
-	var pulsingIcon = L.icon.pulse({iconSize:[15,15],color:'blue'});
-	currentLocationMarker = L.marker([0,0],{icon: pulsingIcon}).addTo(pokemap);
+	// var pulsingIcon = L.icon.pulse({iconSize:[15,15],color:'blue'});
+	// currentLocationMarker = L.marker([0,0],{icon: pulsingIcon}).addTo(pokemap);
 
-	// currentLocationMarker = L.circle([0, 0], 10, {
- //    	color: 'red',
- //    	fillColor: '#f03',
- //    	fillOpacity: 0.5
-	// }).addTo(pokemap);
+	currentLocationMarker = L.circle([0, 0], 10, {
+    	color: 'red',
+    	fillColor: '#f03',
+    	fillOpacity: 0.5
+	}).addTo(pokemap);
 
 	modalLocationMarker = L.circle([0, 0], 10, {
     	color: 'red',
