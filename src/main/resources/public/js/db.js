098322f87e19e2d029a1b481d23abf97de173c7f
Cesar Guerrero <cesarguerrero@Cesars-MacBook-Pro.local>
2016-08-16 01:29:48 -0500

    good start with administrative page

--- src/main/resources/public/js/db.js
+++ src/main/resources/public/js/db.js
@@ -4,20 +4,31 @@ $( document ).ready(function() {
 	// TODO: add check listeners to list items
 	$('.db-list-item').click(function() {
 		
-		// TODO: extract data from DOM element
-		console.log(this.children[0].innerText);
+		// TODO: extract nest id from selected DOM element
 		var id = (this.children[0].innerText.split(':')[1]).trim();
-		console.log("selected nest id: '" + id +"'");
+
+		// TODO: extract user's password from saved cookies
+		var password = getCookie("passwd");
 
 		// TODO: on click display nest details modal
 
 		// TODO: give option to delete if password is stored in a cookie
-
+		console.log(id);
+		console.log(password);
 	});
-	
-	
-
-	
-
-	
 });
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
+}
