f15b663c0d483929988d344fe8490efbd9f2e6de
Cesar Guerrero <cesarguerrero@Cesars-MacBook-Pro.local>
2016-08-16 01:19:43 -0500

    figured it out

--- src/main/resources/public/js/db.js
+++ src/main/resources/public/js/db.js
@@ -5,10 +5,8 @@ $( document ).ready(function() {
 	$('.db-list-item').click(function() {
 		
 		// TODO: extract data from DOM element
-		console.log(this);
-		console.log(this.children);
-		console.log(this.firstChild);
-		
+		console.log(this.children[0].innerText);
+
 		// TODO: on click display nest details modal
 
 		// TODO: give option to delete if password is stored in a cookie
