09d0cef0e0ac0548bfe53f6803a6cfacfa937c4b
Cesar Guerrero <cesarguerrero@Cesars-MacBook-Pro.local>
2016-08-16 01:24:03 -0500

    simple extract

--- src/main/resources/public/js/db.js
+++ src/main/resources/public/js/db.js
@@ -6,9 +6,7 @@ $( document ).ready(function() {
 		
 		// TODO: extract data from DOM element
 		console.log(this.children[0].innerText);
-
-		var extract = this.children[0].innerText;
-		var id = (str.split(':')[1]).trim();
+		var id = (this.children[0].innerText.split(':')[1]).trim();
 		console.log("selected nest id: '" + id +"'");
 
 		// TODO: on click display nest details modal
